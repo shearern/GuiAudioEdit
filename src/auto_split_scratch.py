28f270653d2982333d87272a52820c468649f7c2
Nathan Shearer <shearern@gmail.com>
2018-01-29 08:51:38 -0800

    Implemented track splitting!

--- src/auto_split_scratch.py
+++ src/auto_split_scratch.py
@@ -81,8 +81,6 @@ class AudioFrame(object):
         return tuple([self._value_1(v) for v in self.values])
 
 
-
-
 class StdoutWrapper(object):
     def __init__(self, fp):
         self.fp = fp
@@ -119,7 +117,9 @@ def get_raw_audio_data(path):
 
     cmd = (
         '-i', path,
-        '-f', 'wav', '-',
+        '-f', 'wav',
+        '-ac', '1', # Mix down to mono
+        '-', # Output to stdout
     )
 
     p = start_ffmpeg(cmd)
@@ -207,66 +207,141 @@ class SilencePeriod(object):
     def __init__(self, start):
         self.start = start
         self.end = start
+    def __str__(self):
+        return '%s to %s' % (self.start.ts, self.end.ts)
+    @property
+    def length(self):
+        return self.end.ts - self.start.ts
+    @property
+    def frames(self):
+        return self.end.frame_num - self.start.frame_num + 1
 
 
 def find_silent_sections(path):
+    '''
+    Find periods of silence in the audio file
+    
+    Note: Assumes mono input (makes analysis much easier)
+    
+    :param path: Path to audio file 
+    :return: Generator of SilencePeriod
+    '''
 
     moving_avg_sec = 15
     min_silence_len = timedelta(seconds=0.25)
 
+    # Setup moving average get average wave values
+    avg = None
+    period = None
+    fill_frames = None
+
     for frame in get_raw_audio_data(path):
+
+        # if frame.pos.frame_num % (44110 * 15) == 0:
+        #     print(str(frame.pos))
 
         # Init
         if frame.pos.frame_num == 0:
-
             fmt = frame.fmt
-
-            ch = [{
-                'moving_avg': MovingAverage(fmt.framerate*moving_avg_sec),  # Setup moving average get average wave values
-                'period': None,
-                }, ] * fmt.num_channels
-
             fill_frames = moving_avg_sec * fmt.framerate
+            avg = MovingAverage(fmt.framerate*moving_avg_sec)
 
         # Normalize values from 0.0 to 1.0
-        sample = [abs(meassure) for meassure in frame.values_1]
+        sample = abs(frame.values_1[0])
 
         # Consume initial seconds to fill moving average
         # (note: won't detect silence in this first window)
         if frame.pos.frame_num < fill_frames:
-            for c in fmt.num_channels:
-                ch[c]['moving_avg'].add(sample[c])
+            avg.add(sample)
 
         # Rest of the file
         else:
-            for c in fmt.num_channels:
-                silence_threshold = ch[c]['moving_avg'].avg * 0.05
-                is_silent = sample[c] < silence_threshold
-                if is_silent:
-                    if ch[c]['period'] is None:
-                        ch[c]['period'] = SilencePeriod(start=frame.pos)
-                    else:
-                        ch[c]['period'].end = frame.pos
+            silence_threshold = avg.avg * 0.15
+            is_silent = sample < silence_threshold
+            if is_silent:
+                if period is None:
+                    period = SilencePeriod(start=frame.pos)
                 else:
-                    if ch[c]['period'] is not None:
-                        plength = ch[c]['period'].end.ts - ch[c]['period'].start.ts
-                        if plength >= min_silence_len:
-                            yield ch[c]['period']
-                    ch[c]['period'] = None
+                    period.end = frame.pos # Extend period to include this frame
+            else: # Not silent
+                if period is not None:
+                    plength = period.end.ts - period.start.ts
+                    if plength >= min_silence_len:
+                        yield period
+                period = None
 
+
+class Track(SilencePeriod):
+    def __init__(self, start, track_num):
+        super(Track, self).__init__(start)
+        self.track_num = track_num
+
+
+def calc_track_times(path, min_track_len, max_track_len):
+    '''
+    Do calculations to split tracks into sizes picking silent periods to split
+
+    Will pick the longest silence between minimum and maximum lengths.
+
+    :param path:
+    :param min_track_len: Minimun length of a track (timedelta)
+    :param max_track_len:  Maximium length of track (timedelta)
+    :return: Tracks
+    '''
+    silences = find_silent_sections(path)
+
+    # Get first silense to get format
+    first_silence = silences.next()
+    framerate = first_silence.start.frames_per_sec
+
+    # Start first track
+    start = AudioTime(0, framerate)
+    track = Track(start, 1)
+
+    while True:
+
+        track_min_end = track.start.ts + min_track_len
+        track_max_end = track.start.ts + max_track_len
+
+        try:
+            silences_for_split = list()
+            for silence in silences:
+                if silence.start.ts > track_min_end:
+                    silences_for_split.append(silence)
+                if silence.end.ts > track_max_end:
+                    break
+
+            longest_silence = max(silences_for_split, key=lambda s: s.length)
+            mid_frame_nun = longest_silence.start.frame_num + (longest_silence.frames / 2)
+            split_at = AudioTime(mid_frame_nun, framerate)
+
+            track.end = split_at
+            yield track
+
+            track = Track(split_at, track.track_num+1)
+
+        except StopIteration:
+
+            # No more silences.  Assuming end of audio file
+            track.end = None
+            yield track
+            return
 
 if __name__ == '__main__':
 
     # First, find average value in signal
     path = r"C:\Users\nshearer\Downloads\2018-01-07 Adult Bible Study - rec (partial).flac"
+    path = r"G:\User Files\Dropbox\SBC Recordings\Archive\2018\2018-01-07 Adult Bible Study\2018-01-07 Adult Bible Study - rec (partial).flac"
 
-    for frame in get_raw_audio_data(path):
+    for track in calc_track_times(path, timedelta(minutes=7), timedelta(minutes=10)):
+        print("%d  %s - %s" % (track.track_num, track.start, track.end))
 
-        if frame.pos.frame_num % (44100 * 15) == 0:
-            print str(frame.pos)
 
-        # For the first 5 seconds, just add the value
-        #moving_avg.add(abs(frame.values_1[1]))
-
-        if frame.pos.frame_num > (44100 * 60 * 2):
-            break
+        # if frame.pos.frame_num % (44100 * 15) == 0:
+        #     print str(frame.pos)
+        #
+        # # For the first 5 seconds, just add the value
+        # #moving_avg.add(abs(frame.values_1[1]))
+        #
+        # if frame.pos.frame_num > (44100 * 60 * 2):
+        #     break
