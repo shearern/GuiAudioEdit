b4b6c6bc708a4a02212c54ba05b080b2d11150be
Nathan Shearer <nshearer@ewu.edu>
2018-01-29 17:24:52 -0800

    Rewrote frame iteration

--- src/auto_split_scratch.py
+++ src/auto_split_scratch.py
@@ -4,6 +4,7 @@ import subprocess
 import struct
 import math
 import audioop
+from itertools import islice, izip
 from datetime import timedelta
 
 
@@ -27,6 +28,7 @@ class AudioFrame(object):
         self.data = frame_data
         self.fmt = fmt
         self.pos = pos
+
     @property
     def values(self):
         '''Return the integer values stored in the audio data'''
@@ -90,64 +92,89 @@ class StdoutWrapper(object):
         return self.fp.close()
 
 
-def start_ffmpeg(cmd):
+class AudioFile(object):
 
-    # TODO: Find it
-    ffmpeg = r"ffmpeg.exe"
-
-    cmd = [ffmpeg, ] + list(cmd)
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None)
-
-    return p
-
-
-class AudioFormat(object):
-    def __init__(self):
+    def __init__(self, path):
+        self.path = path
         self.encoding = None
         self.framerate = None
         self.samplewidth = None
         self.num_channels = None
 
 
-def get_raw_audio_data(path):
+    def _start_ffmpeg(self, cmd):
+        # TODO: Find it
+        ffmpeg = r"ffmpeg.exe"
 
-    if not os.path.exists(path):
-        print("ERROR: File doesn't exist: " + path)
-        sys.exit(2)
+        cmd = [ffmpeg, ] + list(cmd)
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=None)
 
-    cmd = (
-        '-i', path,
-        '-f', 'wav',
-        '-ac', '1', # Mix down to mono
-        '-', # Output to stdout
-    )
+        return p
 
-    p = start_ffmpeg(cmd)
 
-    wfh = wave.open(StdoutWrapper(p.stdout), 'r')
+def _nbyte_iterator(data, bytes):
+    '''
+    Yield n bytes at a time from the data
 
-    fmt = AudioFormat()
-    fmt.encoding = os.path.splitext(os.path.basename(path))[1].strip('.')
-    fmt.framerate = wfh.getframerate()
-    fmt.samplewidth = wfh.getsampwidth()
-    fmt.num_channels = wfh.getnchannels()
+    _nbyte_iterator('ABCDEF') --> 'AB', 'CD', 'EF'
 
-    frame_num = 0
+    :param data: Data to slicer
+    :param bytes: How many bytes at once to return
+    '''
 
-    frames_at_once = 4096
-    frame_size = fmt.samplewidth * fmt.num_channels
+    # Trying to use islice in case it's faster than indexing in a list
 
-    while True:
+    # Setup iterators to slice through data
+    i = iter(data)
+    try:
+        while True:
+            yield i.next() + i.next()
+    except StopIteration:
+        return
 
-        # Get more data
-        window = wfh.readframes(frames_at_once)
 
-        # Iterate over frames
-        for pos in range(0, len(window), frame_size):
-            t = AudioTime(frame_num, fmt.framerate)
-            yield AudioFrame(window[pos:pos+(frame_size)], fmt, t)
-            frame_num += 1
+class AudioFileReader(AudioFile):
 
+    def get_raw_audio_data(self):
+
+        if not os.path.exists(self.path):
+            print("ERROR: File doesn't exist: " + self.path)
+            sys.exit(2)
+
+        cmd = (
+            '-i', path,
+            '-f', 'wav',
+            '-ac', '1', # Mix down to mono
+            '-', # Output to stdout
+        )
+
+        p = self._start_ffmpeg(cmd)
+
+        wfh = wave.open(StdoutWrapper(p.stdout), 'r')
+
+        self.encoding = os.path.splitext(os.path.basename(path))[1].strip('.')
+        self.framerate = wfh.getframerate()
+        self.samplewidth = wfh.getsampwidth()
+        self.num_channels = wfh.getnchannels()
+
+        frame_num = 0
+
+        frames_at_once = 1024 * 1024
+        frame_size = self.samplewidth * self.num_channels
+
+        while True:
+
+            # Get more data
+            window = wfh.readframes(frames_at_once)
+            if not window:
+                return
+
+            # Iterate over frames
+            for i, frame_data in enumerate(_nbyte_iterator(window, frame_size)):
+                self.last_frame = AudioFrame(''.join(frame_data), self, AudioTime(frame_num+i, self.framerate))
+                yield self.last_frame
+
+            frame_num += len(window)
 
 
 class MovingAverage(object):
@@ -217,13 +244,13 @@ class SilencePeriod(object):
         return self.end.frame_num - self.start.frame_num + 1
 
 
-def find_silent_sections(path):
+def find_silent_sections(af):
     '''
     Find periods of silence in the audio file
     
     Note: Assumes mono input (makes analysis much easier)
     
-    :param path: Path to audio file 
+    :param af: Audio file to be read
     :return: Generator of SilencePeriod
     '''
 
@@ -235,7 +262,7 @@ def find_silent_sections(path):
     period = None
     fill_frames = None
 
-    for frame in get_raw_audio_data(path):
+    for frame in af.get_raw_audio_data():
 
         # if frame.pos.frame_num % (44110 * 15) == 0:
         #     print(str(frame.pos))
@@ -277,18 +304,18 @@ class Track(SilencePeriod):
         self.track_num = track_num
 
 
-def calc_track_times(path, min_track_len, max_track_len):
+def calc_track_times(af, min_track_len, max_track_len):
     '''
     Do calculations to split tracks into sizes picking silent periods to split
 
     Will pick the longest silence between minimum and maximum lengths.
 
-    :param path:
+    :param af: Audio file
     :param min_track_len: Minimun length of a track (timedelta)
     :param max_track_len:  Maximium length of track (timedelta)
     :return: Tracks
     '''
-    silences = find_silent_sections(path)
+    silences = find_silent_sections(af)
 
     # Get first silense to get format
     first_silence = silences.next()
@@ -323,18 +350,50 @@ def calc_track_times(path, min_track_len, max_track_len):
         except StopIteration:
 
             # No more silences.  Assuming end of audio file
-            track.end = None
+            track.end = af.last_frame
             yield track
             return
+
+
+def split_disks(af):
+    '''Return tracks split into disks'''
+
+    max_disk_time = timedelta(minutes=70)
+
+    # Organize tracks into disks
+    disk_num = 1
+    track_num = 1
+    disk_end_by = max_disk_time
+    for track in calc_track_times(af, min_track_len=timedelta(minutes=7), max_track_len=timedelta(minutes=10)):
+        if track.end.ts <= disk_end_by:
+            track.disk_num = disk_num
+            track.track_num = track_num
+            yield track
+            track_num += 1
+        else:
+            disk_start = track.start.ts
+            disk_end_by = disk_start + max_disk_time
+
+            disk_num += 1
+            track.disk_num = disk_num
+
+            track_num = 1
+            track.track_num = track_num
+
+            yield track
+
 
 if __name__ == '__main__':
 
     # First, find average value in signal
     path = r"C:\Users\nshearer\Downloads\2018-01-07 Adult Bible Study - rec (partial).flac"
-    path = r"G:\User Files\Dropbox\SBC Recordings\Archive\2018\2018-01-07 Adult Bible Study\2018-01-07 Adult Bible Study - rec (partial).flac"
+    #path = r"G:\User Files\Dropbox\SBC Recordings\Archive\2018\2018-01-07 Adult Bible Study\2018-01-07 Adult Bible Study - rec (partial).flac"
+    af = AudioFileReader(path)
 
-    for track in calc_track_times(path, timedelta(minutes=7), timedelta(minutes=10)):
-        print("%d  %s - %s" % (track.track_num, track.start, track.end))
+    for track in split_disks(af):
+        print "%s - %s\tD%02d T%02d" % (str(track.start), str(track.end), track.disk_num, track.track_num)
+
+
 
 
         # if frame.pos.frame_num % (44100 * 15) == 0:
